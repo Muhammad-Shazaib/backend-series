3efd29f3d1b9e28b86241c31e4d8e80d85525ca2
muhammad-shazaib <shazaibirshad126@gamil.com>
2025-08-15 23:26:03 +0500

    Remove extra folders inside project

--- project/Hospital Managment System/medical_record.modules.js
+++
@@ -1,5 +0,0 @@
-import mongoose from "mongoose";
-
-const medicalRecordSchema = new mongoose.Schema({} , {timestamps: true,});
-
-export const medicalRecord = mongoose.model('medicalRecord', medicalRecordSchema);
