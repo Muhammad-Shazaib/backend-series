d2324fabdf52e810ddd89bfa11ceb1f956312967
muhammad-shazaib <shazaibirshad126@gamil.com>
2025-08-15 20:55:04 +0500

    under process hospital managment

--- Hospital Managment System/patients.modules.js
+++ Hospital Managment System/patients.modules.js
@@ -1,1 +1,19 @@
+import mongoose from "mongoose";
 
+const patientSchema = new mongoose.Schema({
+name: {
+    type: String,
+    required: true,
+},
+diagonsedWith: {
+    type: String,
+    required: true,
+},
+gender: {
+    type: String,
+    required: true,
+}
+
+}, {timestamps: true,} );
+
+export const patient = mongoose.model('patient', patientSchema);
